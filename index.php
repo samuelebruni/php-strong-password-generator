4ddd22ae2b2659fdbb5387a030b3c64587b08acf
samuelebruni <samuele_bruni@hotmail.com>
2023-10-11 14:39:49 +0200

    create with bootstrap layout

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Password Generator</title>
 </head>
-<body>
-    
+<body class="bg-success">
+    <div class="container d-flex flex-column align-items-center justify-content-center">
+        <h1 class="py-5 px-2">Genera una Password sicura!</h1>
+        <div class="mb-3 w-50 d-flex flex-column align-items-center gap-2">
+          <label for="" class="form-label fw-bold">Inserisci il numero di caratteri che avrà la tua password</label>
+          <input type="text" name="" id="" class="form-control text-center" placeholder="Digita qui il numero dei caratteri.." aria-describedby="helpId">
+            <input name="" id="" class="btn btn-danger" type="button" value="Vai">
+        </div>
+    </div>
 </body>
 </html>
